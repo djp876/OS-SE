e587bb0e05304a0b401bb3719d1410e5dd7bfe30
DJP <79404155+djp876@users.noreply.github.com>
2022-10-28 05:49:42 +0530

    Update 6_c_First_fit.c

--- 6_c_First_fit.c
+++ 6_c_First_fit.c
@@ -1,60 +1,52 @@
-//6_c_First-fit
 #include<stdio.h>
 #define max 25
-void main()
+int main()
 {
-int 
-frag[max],b[max],f[max],i,j,nb,nf,temp,highest=0; static int bf[max],ff[max];
-printf("\n\tMemory Management Scheme - Worst Fit"); 
-printf("\nEnter the number of blocks:");
+int frag[max],b[max],f[max],i,j,nb,nf,temp;
+static int bf[max],ff[max];
+printf("Enter the number of blocks:");
 scanf("%d",&nb);
-printf("Enter the number of files:"); 
+printf("Enter the number of files:");
 scanf("%d",&nf);
-printf("\nEnter the size of the blocks:-\n");
+printf("Enter the size of the blocks:\n");
 for(i=1;i<=nb;i++)
-{
-printf("Block %d:",i);
 scanf("%d",&b[i]);
-}
-printf("Enter the size of the files :-\n"); 
+printf("Enter the size of the files :\n");
 for(i=1;i<=nf;i++)
-{
-printf("File %d:",i);
 scanf("%d",&f[i]);
-}
 for(i=1;i<=nf;i++)
 {
 for(j=1;j<=nb;j++)
 {
-if(bf[j]!=1) //if bf[j] is not allocated
+if(bf[j]!=1)
 {
-temp=b[j]-f[i]; 
+temp=b[j]-f[i];
 if(temp>=0)
-if(highest<temp)
 {
+ff[i]=j;
+break;
 }
 }
-frag[i]=highest; bf[ff[i]]=1; highest=0;
 }
-ff[i]=j; highest=temp;
+frag[i]=temp;
+bf[ff[i]]=1;
 }
-printf("\nFile_no:\tFile_size:\tBlock_no:\tBlock_size:\tFragement"); 
+printf("\nFile_no:\tFile_size :\tBlock_no:\tBlock_size:\tFragement");
 for(i=1;i<=nf;i++)
 printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d",i,f[i],ff[i],b[ff[i]],frag[i]);
 }
 /*
-Output:
-Memory Management Scheme - Worst Fit
 Enter the number of blocks:3
 Enter the number of files:2
-Enter the size of the blocks:-
-Block 1:5
-Block 2:2
-Block 3:7
-Enter the size of the files :-
-File 1:1
-File 2:4
-File_no:	File_size:	Block_no:	Block_size:	Fragement
-1		1		4		0		0
-2		4		4		0		0
+Enter the size of the blocks:
+5
+2
+7
+Enter the size of the files :
+1
+4
+
+File_no:        File_size :     Block_no:       Block_size:     Fragement
+1               1               1               5               4
+2               4               3               7               3
 */
